6daf1e6692fdfc6562b5e19ebb72a26361ab8277
kupina <kupina@yandex.ru>
2022-08-08 06:19:57 +0300

    Добавила проект Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 12, 2, 33, 4, 51, 67, 70, 87, 87, 9 };
+
+int n = array.Length;
+int find = 87;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+
+}
